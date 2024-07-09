c23c968ac27e0d35b383e50e06f85f3f1cfcc74f
HauCG <maitrunghau9817189@gmail.com>
2024-07-09 15:48:55 +0700

    thembai3

---
+++ bai3.js
@@ -0,0 +1,12 @@
+let a = +prompt('Nhập số', '');
+switch (true) {
+    case a > 1:
+        alert('1');
+        break;
+    case a < 1:
+        alert('-1');
+        break;
+    case a === 1:
+        alert('0');
+        break;
+}
